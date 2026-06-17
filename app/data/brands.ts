652a8b19ffd5dc0b6f7f5f73e0ffdf0510daa2a6
Zakariyya <zakariyya.z@antetechnologies.com>
2026-06-17 10:37:54 +0400

    new

--- app/data/brands.ts
+++ app/data/brands.ts
@@ -10,39 +10,39 @@ export interface Brand {
 }
 
 export const brands: Brand[] = [
-  {
-    id: "bananzia",
-    name: "Bananzia",
-    logo: "/logos/bananzia.svg",
-    rating: 10,
-    bonus: "Casino Welcome Bonus 300% up to £3000 + 300 FS!",
-    url: "https://go.drctvoodoo.com/click?pid=6&offer_id=12&sub2=Bananzia-GA02&sub1=",
-    isMobile: true,
-    votes: 1340,
-  },
+  // {
+  //   id: "bananzia",
+  //   name: "Bananzia",
+  //   logo: "/logos/bananzia.svg",
+  //   rating: 10,
+  //   bonus: "Casino Welcome Bonus 300% up to £3000 + 300 FS!",
+  //   url: "https://go.drctvoodoo.com/click?pid=6&offer_id=12&sub2=Bananzia-GA02&sub1=",
+  //   isMobile: true,
+  //   votes: 1340,
+  // },
   
 
-  {
-    id: "spinpolo",
-    name: "SpinPolo",
-    logo: "/logos/spinpolo.webp",
-    rating: 9.8,
-    bonus: "777% Up To £7777 + 385FS + 300k Chips",
-    url: "https://direct.reradirectbox.com/click?pid=75&offer_id=857&l=1778754174&sub2=SpinPolo-GA02&sub1=",
-    isMobile: true,
-    votes: 1105,
-  },
+  // {
+  //   id: "spinpolo",
+  //   name: "SpinPolo",
+  //   logo: "/logos/spinpolo.webp",
+  //   rating: 9.8,
+  //   bonus: "777% Up To £7777 + 385FS + 300k Chips",
+  //   url: "https://direct.reradirectbox.com/click?pid=75&offer_id=857&l=1778754174&sub2=SpinPolo-GA02&sub1=",
+  //   isMobile: true,
+  //   votes: 1105,
+  // },
 
-  {
-    id: "daytonaspin",
-    name: "DaytonaSpin",
-    logo: "/logos/daytonaspin.svg",
-    rating: 9.5,
-    bonus: "255% Up To 4500 £ + 255 FS",
-    url: "https://direct.reradirectbox.com/click?pid=75&offer_id=345&l=1774277506&sub2=DaytonaSpin-GA02&sub1=",
-    isMobile: true,
-    votes: 1340,
-  },
+  // {
+  //   id: "daytonaspin",
+  //   name: "DaytonaSpin",
+  //   logo: "/logos/daytonaspin.svg",
+  //   rating: 9.5,
+  //   bonus: "255% Up To 4500 £ + 255 FS",
+  //   url: "https://direct.reradirectbox.com/click?pid=75&offer_id=345&l=1774277506&sub2=DaytonaSpin-GA02&sub1=",
+  //   isMobile: true,
+  //   votes: 1340,
+  // },
   
   {
     id: "grosvenor-casinos",
